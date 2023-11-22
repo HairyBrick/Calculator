39bc7269bbf7d8bb832d2f0145c727849ea9ad35
HairyBrick <150380574+HairyBrick@users.noreply.github.com>
2023-11-22 15:41:27 +0300

    Add files via upload

---
+++ CalculatorAdd.pas
@@ -0,0 +1,10 @@
+﻿var num1, num2: integer;
+begin
+  writeln('==Кальулятор сложения==');
+  
+  write('Напишите первое число: ');
+  readln(num1);
+  write('Напишите второе число: ');
+  readln(num2);
+  write('Ответ: ', num1+num2);
+end.
